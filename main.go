f1d246d9d4b5bf19615993749da9159c8b23d047
Fuse <jiranon.khemklad@gmail.com>
2026-04-09 11:37:43 +0700

    Implement HTTP server with routes for health and message
    Added HTTP server with multiple routes including health check and API message response.

--- main.go
+++ main.go
@@ -1,6 +1,40 @@
 package main
-import "fmt"
 
-func main(){
-	fmt.Println("Hello, World! My name is SMP-Context, and I am a simple Go program.")
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// response struct
+type Message struct {
+	Message string `json:"message"`
+}
+
+func main() {
+
+	// route: /
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World! My name is SMP-Context 🚀")
+	})
+
+	// route: /health
+	http.HandleFunc("/_status/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "OK")
+	})
+
+	// route: /api/message
+	http.HandleFunc("/api/message", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		res := Message{
+			Message: "Hello from Go API 🔥",
+		}
+
+		json.NewEncoder(w).Encode(res)
+	})
+
+	fmt.Println("Server running on :3000")
+	http.ListenAndServe(":3000", nil)
 }
